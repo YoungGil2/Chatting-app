5b086b8173e8b7750427efa31bb76538d6d791f1
yg.kim <yg.kim@dnd-lab.com>
2021-09-02 15:52:54 +0900

    heroku build filed 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Join  from "./pages/Join";
-import Room  from "./pages/Room";
-import Header from "./layout/Header"
+
 
 function App() {
   return (
-    <>
-      <Header />
+    <>fsdfsdfsd
+    
       <BrowserRouter>
         <Switch>
-          <Route excact path="/join" component={Join} />
-          <Route path="/room" component={Room} />
+        ㄹㄴㅇㄹㄴㅇㄹㄴㅇㄹ
         </Switch>
       </BrowserRouter>
     </>
