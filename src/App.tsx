5d37666107a1fcb30cfafd2c5b1808fe4ce54bdd
yg.kim <yg.kim@dnd-lab.com>
2021-09-02 15:40:28 +0900

    package lock 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <Header />
       <BrowserRouter>
         <Switch>
-    
+          <Route excact path="/join" component={Join} />
+          <Route path="/room" component={Room} />
         </Switch>
       </BrowserRouter>
     </>
