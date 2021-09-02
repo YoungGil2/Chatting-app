b7e2506645be2b5a3a49a79903ec6a087a02e987
yg.kim <yg.kim@dnd-lab.com>
2021-09-02 15:30:35 +0900

    heroku test

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App() {
       <Header />
       <BrowserRouter>
         <Switch>
-          <Route excact path="/join" component={Join} />
-          <Route path="/room" component={Room} />
+    
         </Switch>
       </BrowserRouter>
     </>
