5985db22447808906c5e2d18ddfa59d59ebbbf3a
yg.kim <yg.kim@dnd-lab.com>
2021-09-02 16:35:40 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Join  from "./pages/Join";
+import Room  from "./pages/Room";
+import Header from "./layout/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route excact path="/join" component={Join} />
+          <Route path="/room" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
