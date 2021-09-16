effc0d1ac7b0de2c4e9cfc65cbdeb2d62a862243
yg.kim <yg.kim@dnd-lab.com>
2021-09-16 15:55:12 +0900

    srver Path 수정

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,7 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
-const Server = "https://react-chatting-project.herokuapp.com/";
+const Server = "/";
 let socket;
 
 function Room({location}) {
