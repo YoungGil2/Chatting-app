c13fc65d2f0fabf81b73bdf4a4897efd20225558
yg.kim <yg.kim@dnd-lab.com>
2021-09-26 23:57:36 +0900

    메시지 작성 component 작업

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,12 +1,11 @@
 import queryString from "query-string";
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import socketClient from "socket.io-client";
-import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import List from "../component/List";
 import Message from "../component/Message";
+import Input from "../component/Input";
 
 const Server = "/";
 let socket;
@@ -17,6 +16,7 @@ function Room({location}) {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
+  const messagesRef = useRef<HTMLDivElement>(null);
  
   useEffect(()=>{
     socket = socketClient(Server);
@@ -44,40 +44,43 @@ function Room({location}) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
 
+  useEffect(() => {
+    messagesRef.current!.scrollTop = messagesRef.current!.scrollHeight;
+  }, [messages]);
+
   const sendMessage = (e)=> {
-    if(message && e.key === "Enter"){
+    e.preventDefault();
+    if(message){
       socket.emit("sendMessage",name, message,()=>{setMessage("")});
     }
   };
   return (
     <Grid container justifyContent="center" style={{ padding: "1em" }}>
-       <Grid item xs={12} sm={9} md={7}>
-        <Paper>
-          <List ListData={users}/>
-        </Paper>
-      </Grid>
-    
       <Grid item xs={12} sm={9} md={7}>
         <Paper>
-          {messages.map((value, key)=> {
-            // const { name, message } = value;
-            return (
-              <Message key={key} name={value.name} text={value.message}  user={name}/>
-            );
-          })}
-          <Grid container justifyContent="center" >
-              <TextField
-                variant="outlined"
-                value={message}
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                }}
-                onKeyPress={(e) => {
-                  sendMessage(e);
-                }}
-              />
-              <Button variant="contained" onClick={()=>{ socket.emit("sendMessage",name, message,()=>{setMessage("")});}}>전송</Button>
-            </Grid>
+          <List ListData={users} />
+        </Paper>
+      </Grid>
+
+      <Grid item xs={12} sm={9} md={7} style={{ marginTop: "10px"}}>
+        <Paper>
+          <div style={{ height: "400px", overflowY: "auto" }} ref={messagesRef}>
+            {messages.map((value, key) => {
+              return (
+                <Message
+                  key={key}
+                  name={value.name}
+                  text={value.message}
+                  user={name}
+                />
+              );
+            })}
+          </div>
+          <Input
+            sendMessage={sendMessage}
+            message={message}
+            setMessage={setMessage}
+          />
         </Paper>
       </Grid>
     </Grid>
